fa8cdfc7adba5b390e9e039360b4f3c07bef884a
jose AZ <131062746+tallerAZ@users.noreply.github.com>
2024-10-04 20:05:49 -0500

    Update script.js

--- proyecto1_a/script.js
+++ proyecto1_a/script.js
@@ -61,7 +61,7 @@ async function fetchStarChart(latitude, longitude) {
                         declination: 0
                     }
                 },
-                zoom: 8
+                zoom: 6
             }
         }
     });
@@ -181,7 +181,7 @@ document.addEventListener("DOMContentLoaded", () => {
     starChartImage.style.transform = "translateX(-50%)";
     starChartImage.style.display = "block"; // Image should be initially set
     starChartImage.style.visibility = "hidden"; // Hide it initially
-    starChartImage.style.width = "800px"; // Set width to 800px
-    starChartImage.style.height = "300px"; // Set height to 800px
+    starChartImage.style.width = "400px"; // Set width to 800px
+    starChartImage.style.height = "400px"; // Set height to 800px
     document.body.appendChild(starChartImage);
 });
